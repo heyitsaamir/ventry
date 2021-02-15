f675b42e3411ae11a61da52fe8ba58076807bb78
Aamir <ajawaid191@gmail.com>
2021-02-15 08:15:56 -0800

    Change the minifier

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   transformer: {
     assetPlugins: ["expo-asset/tools/hashAssetFiles"],
+    minifierPath: 'metro-minify-terser',
+    minifierConfig: {
+        // https://www.npmjs.com/package/terser#mangle-options
+        ecma: 8,
+        keep_classnames: true,
+        keep_fnames: true,
+        module: true,
+        mangle: {
+          module: true,
+          keep_classnames: true,
+          keep_fnames: true,
+      }
+    }
   },
 };
